f72a573ef804aa60168d5c6d424f0fd3ef1480a8
korice <hung20020307@gmail.com>
2025-01-08 09:52:36 +0800

    EChart sticky

--- src/component/Sidebar/index.jsx
+++ src/component/Sidebar/index.jsx
@@ -5,8 +5,7 @@ import './index.css';
 const Sidebar = () => {
     const [isSidebarVisible, setIsSidebarVisible] = useState(true);
     const [open200, setOpen200] = useState(false);
-    const [open300, setOpen300] = useState(false);
-    const [openISO, setOpenISO] = useState(false); // 新增 ISO 下拉選單的狀態
+    const [openISO, setOpenISO] = useState(false); // ISO 下拉選單的狀態
 
     const toggleSidebar = () => {
         setIsSidebarVisible(!isSidebarVisible);
@@ -16,12 +15,8 @@ const Sidebar = () => {
         setOpen200(!open200);
     };
 
-    const toggleDropdown300 = () => {
-        setOpen300(!open300);
-    };
-
     const toggleDropdownISO = () => {
-        setOpenISO(!openISO); // 切換 ISO 下拉選單
+        setOpenISO(!openISO);
     };
 
     return (
@@ -32,11 +27,11 @@ const Sidebar = () => {
                     <ul>
                         <li><Link to="/ExamplePage">首頁</Link></li>
                         <li>
-                            <div onClick={toggleDropdown200}>
-                                GRI
+                            <div onClick={toggleDropdown200} className="dropdown-header">
+                                GRI {open200 ? '▲' : '▼'}
                             </div>
                             {open200 && (
-                                <ul className="dropdown">
+                                <ul>
                                     <li><Link to="/GRI200">GRI200</Link></li>
                                     <li><Link to="/GRI300">GRI300</Link></li>
                                     <li><Link to="/GRI400">GRI400</Link></li>
@@ -44,11 +39,11 @@ const Sidebar = () => {
                             )}
                         </li>
                         <li>
-                            <div onClick={toggleDropdownISO}>
-                                ISO
+                            <div onClick={toggleDropdownISO} className="dropdown-header">
+                                ISO {openISO ? '▲' : '▼'}
                             </div>
                             {openISO && (
-                                <ul className="dropdown">
+                                <ul>
                                     <li><Link to="/ISO-14064">ISO-14064</Link></li>
                                     <li><Link to="/ISO-14067">ISO-14067</Link></li>
                                     <li><Link to="/ISO-27001">ISO-27001</Link></li>
